1c5197f25935bc8d0d8742c0f70c1c8c232ae9d3
Yahyarashid <yahyomar63@gmail.com>
2019-03-07 15:14:56 +0200

    add picture to university profile

--- client/src/components/courses/coursecard.js
+++ client/src/components/courses/coursecard.js
@@ -93,7 +93,7 @@ class MediaControlCard extends Component {
           </div>
           <CardMedia
             className={classes.cover}
-            image={require("../../images/yarmouk.jpg")}
+            image={require("https://admreg.yu.edu.jo/en/images/stories/yu_langcenter01.jpg")}
             title="Yarmouk University"
           />
         </Card>

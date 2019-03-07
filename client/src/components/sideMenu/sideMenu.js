26f8c0ee00fbb86e8de7862a11a7c8e3ce22377a
Yahyarashid <yahyomar63@gmail.com>
2019-03-07 19:34:37 +0200

    menu

--- client/src/components/sideMenu/sideMenu.js
+++ client/src/components/sideMenu/sideMenu.js
@@ -37,22 +37,14 @@ class TemporaryDrawer extends React.Component {
     const sideList = (
       <div className={classes.list}>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Countries', 'Universities', 'Departments', 'Courses'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+       
       </div>
     );
 
